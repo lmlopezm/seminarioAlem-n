71d78c212f5333669dfc4f551ca2878cb289d4aa
aulamovil09 <aulamovil09@UBAULAM09-25.salas.edu.co>
2019-09-11 07:35:34 -0500

    holi

--- LopezSerratoSeminario/ub/controlador/Principal.java
+++ LopezSerratoSeminario/ub/controlador/Principal.java
@@ -4,8 +4,9 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		@SuppressWarnings("unused")
-		Controlador controlador = new Controlador();
+		System.out.println("holi");
+		//@SuppressWarnings("unused")
+		//Controlador controlador = new Controlador();
 
 	}
 
